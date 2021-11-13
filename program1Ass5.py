74a59652f6e9b8c14e130932470cc0043a3023f5
Rex-Imman Robles <reximman03@gmail.com>
2021-11-13 23:58:50 +0800

    Grade/mark 2.75 satisfactory

--- program1Ass5.py
+++ program1Ass5.py
@@ -14,3 +14,19 @@ elif grade >=91 and grade <= 93:
 elif grade >= 88 and grade <= 90:
     print("Grade/Mark: 1.75")
     print("Description: Very Good")
+    
+if grade >= 85 and grade <= 87:
+    print("Grade/Mark: 2.0")
+    print("Description: Good")
+
+elif grade >=82 and grade <= 84:
+    print("Grade/Mark: 2.25")
+    print("Description: Good")
+    
+elif grade >=79 and grade <= 81:
+    print("Grade/Mark: 2.5")
+    print("Description: Satisfactory")
+    
+elif grade >= 76 and grade <= 78:
+    print("Grade/Mark: 2.75")
+    print("Description: Satisfactory")
