1a8f01fa9cddd97c10354ae81525cfbbcb934e26
Rex-Imman Robles <reximman03@gmail.com>
2021-11-13 23:37:36 +0800

    Grade/Mark 1.75 Very Good

--- program1Ass5.py
+++ program1Ass5.py
@@ -2,3 +2,15 @@ grade = float(input("Enter your Grade: "))
 if grade >= 97 and grade <= 100:
     print("Grade/Mark: 1.0")
     print("Description: Excellent")
+
+elif grade >=94 and grade <= 96:
+    print("Grade/Mark: 1.25")
+    print("Description: Excellent")
+    
+elif grade >=91 and grade <= 93:
+    print("Grade/Mark: 1.5")
+    print("Description: Very Good")
+    
+elif grade >= 88 and grade <= 90:
+    print("Grade/Mark: 1.75")
+    print("Description: Very Good")
