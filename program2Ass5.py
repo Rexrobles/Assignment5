0fd3d1759f4445e423e935f17c35dce43ad2fe10
Rex-Imman Robles <reximman03@gmail.com>
2021-11-14 16:09:18 +0800

    Finish

--- program2Ass5.py
+++ program2Ass5.py
@@ -6,4 +6,9 @@ def lowest_of_three():
         print(f"The lowest number among the three is {a}. ")
     elif b < a and b < c:
         print(f"The lowest number among the three is {b}.")
+    else:
+        print(f"The lowest number among the three is {c}.")
+        
+lowest_of_three()
+print("-DONE-")
     
