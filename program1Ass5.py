5b3f4538d95a36ebde57b6039c51489e5bec1551
Rex-Imman Robles <reximman03@gmail.com>
2021-11-16 00:19:21 +0800

    Final commit revise rounding off

--- program1Ass5.py
+++ program1Ass5.py
@@ -1,44 +1,48 @@
-grade = round(float(input("Enter your Grade: ")))
+import math
+def round_half_up(n, decimals=0):
+    multiplier = 10 ** decimals
+    return math.floor(n*multiplier + 0.5) / multiplier
 
+Mark = round(float(input("Enter you mark:")))
+grade = round_half_up (Mark)
 if grade >= 97 and grade <= 100:
-    print("Grade/Mark: 1.0")
-    print("Description: Excellent")
-
-elif grade >=94 and grade <= 96:
-    print("Grade/Mark: 1.25")
-    print("Description: Excellent")
-    
-elif grade >=91 and grade <= 93:
-    print("Grade/Mark: 1.5")
-    print("Description: Very Good")
-    
+    print("Excellent!")
+    print("Your mark is 1.0!")
+elif grade >= 94 and grade <= 96:
+    print("Excellent!")
+    print("Your mark is 1.25!")
+elif grade >= 91 and  grade <= 93:
+    print("Very Good!")
+    print("Your mark is 1.5!")
 elif grade >= 88 and grade <= 90:
-    print("Grade/Mark: 1.75")
-    print("Description: Very Good")
-    
-if grade >= 85 and grade <= 87:
-    print("Grade/Mark: 2.0")
-    print("Description: Good")
-
-elif grade >=82 and grade <= 84:
-    print("Grade/Mark: 2.25")
-    print("Description: Good")
-    
-elif grade >=79 and grade <= 81:
-    print("Grade/Mark: 2.5")
-    print("Description: Satisfactory")
-    
+    print("Very Good!")
+    print("Your mark is 1.75!")
+elif grade >= 85 and grade <= 87:
+    print("Good!")
+    print("Your mark is 2.0!")
+elif grade >= 82 and grade <= 84:
+    print("Good!")
+    print("Your mark is 2.25!")
+elif grade >= 79 and grade <= 81:
+    print("Satisfactory!")
+    print("Your mark is 2.50!")
 elif grade >= 76 and grade <= 78:
-    print("Grade/Mark: 2.75")
-    print("Description: Satisfactory")
-    
+    print("Satisfactory!")
+    print("Your mark is 2.75!")
 elif grade == 75:
-    print("Grade/Mark: 3.0")
-    print("Description: passing")
-    
-elif grade >=65 and grade <=74:
-    print("Grade/Mark: 5.0")
-    print("Description: Failure, Better luck next time")
+    print("Passing!")
+    print("Your mark is 3.0!")
+elif grade >= 65 and grade <= 74:
+    print("Failure!")
+    print("Your mark is 5.0!")
+
+grade = input("Enter your the status of you mark (Inc.,W,D):")
+if grade == "Inc.":
+        print("Your status on this subject is INCOMPLETE.")
+elif grade == "W":
+        print("Your status on this subject is WITHDRAWN")
+elif grade == "D":
+        print("Your status on this subject is DROPPED")
+
+print("Your grade is recorded!")
    
-else:
-    print("Your grade is either Inc. means Incomplete, W means Withdrawn, or D means Dropped")
