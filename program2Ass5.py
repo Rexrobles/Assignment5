f613f9b66f089ccacb4d96833a0758562d33f350
Rex-Imman Robles <reximman03@gmail.com>
2022-01-09 23:36:13 +0800

    renaming the variable

--- program2Ass5.py
+++ program2Ass5.py
@@ -1,13 +1,13 @@
 def lowest_of_three():
-    a = int(input("First Number: "))
-    b = int(input("Second Number: "))
-    c = int(input("Thrid Number: "))
-    if a < b and a < c:
-        print(f"The lowest number among the three is {a}. ")
-    elif b < a and b < c:
-        print(f"The lowest number among the three is {b}.")
+    Firstnum = int(input("First Number: "))
+    Secondnum = int(input("Second Number: "))
+    Thirdnum = int(input("Thrid Number: "))
+    if Firstnum < Secondnum and Firstnum < Thirdnum:
+        print(f"The lowest number among the three is {firstnum}. ")
+    elif Secondnum < Firstnum and Secondnum < Thirdnum:
+        print(f"The lowest number among the three is {Secondnum}.")
     else:
-        print(f"The lowest number among the three is {c}.")
+        print(f"The lowest number among the three is {Thirdnum}.")
         
 lowest_of_three()
 print("-DONE-")
